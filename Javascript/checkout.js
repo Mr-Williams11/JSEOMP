a364c136c7b6f674fadb7261a1fadd604a69f4ff
Mr-Williams11 <joelwilliams742@gmail.com>
2023-12-06 16:29:45 +0200

    productcheck

--- Javascript/checkout.js
+++ Javascript/checkout.js
@@ -1,14 +1,13 @@
 let purchased = JSON.parse(localStorage.getItem('storeitems'))
 
 let table = document.querySelector('table')
-table.innerHTML= purchased.map((item,index)=>{
+table.innerHTML= purchased.map((car,index)=>{
     return `
     <tr>
-    <td>${index+1}</td>
-    <td>${item.name}</td>
-    <td>${item.description}</td>
-    <td>R${item.price}</td>
-    <td><img src='${item.url}' height="70px" width="80px"></img></td>
+    <td>${car.carname}</td>
+    <td>${car.model}</td>
+    <td>R${car.price}</td>
+    <td><img src='${car.url}' height="70px" width="80px"></img></td>
     </tr>
     `
 })

31b58c5e8604f79cb9f6cec6423c04d690fd0a78
Mr-Williams11 <joelwilliams742@gmail.com>
2023-12-08 12:50:47 +0200

    almost

--- Javascript/admin.js
+++ Javascript/admin.js
@@ -1,5 +1,5 @@
 // this is where all products will be stored
-let dealership = []
+let dealership = [];
 
 // function to create objects
 function Carconstructor(carname, model, year, price, url) {
@@ -17,76 +17,190 @@ let car3 = new Carconstructor("Nissan", "Skyline", 2021, 32000, "https://i.posti
 let car4 = new Carconstructor("Audi", "A4", 2023, 55000, "https://i.postimg.cc/4Nc2nsrh/The-Audi-S4-Is-Relentless-Precise-and-Way-Better-Looking-for-2020.png");
 let car5 = new Carconstructor("Toyota", "Supra MK4", 2022, 25000, "https://i.postimg.cc/rmhFRx0Q/widebody-toyota-supra.png");
 
-
-
 // pushing items into array
-dealership.push(car1,car2,car3,car4,car5)
+dealership.push(car1, car2, car3, car4, car5);
 
-//sets array from the local storage tp array(items) in code
-//JSON.parse turns the string into an object
-localStorage.setItem('dealership',JSON.stringify(dealership))
-dealership = JSON.parse(localStorage.getItem('dealership'))
+// sets array from the local storage to array(items) in code
+// JSON.parse turns the string into an object
+localStorage.setItem('dealership', JSON.stringify(dealership));
+dealership = JSON.parse(localStorage.getItem('dealership'));
 
+let table = document.querySelector('table');
 
-
-let table = document.querySelector('table')
 function joel() {
     let products = dealership.map(function (car, index) {
-        console.log(car)
-        console.log(index)
         return `
-
-        <div class="table-responsive">
-  <table class="table">
-        <thead>
-            <tr>
-                <th>Car Name</th>
-                <th>Model</th>
-                <th>Year</th>
-                <th>Price</th>
-                <th>Image</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>${car.carname}</td>
-                <td>${car.model}</td>
-                <td>${car.year}</td>
-                <td>R ${car.price}</td>
-                <td><img src="${car.url}" height="70px" width="80px"></td>
-            </tr>
-            <tr>
-            <td><button class='edit'>Edit</button></td>
-            <td><button class='delete' value='${index}'>Del</button></td>
-            </tr>
-        </tbody>
+        <div class="table-responsive-lg">
+        <table class="table table-bordered table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Car Name</th>
+                    <th>Model</th>
+                    <th>Year</th>
+                    <th>Price</th>
+                    <th>Image</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>${car.carname}</td>
+                    <td>${car.model}</td>
+                    <td>${car.year}</td>
+                    <td>R ${car.price}</td>
+                    <td><img src="${car.url}" height="70px" width="80px" class="img-fluid" alt="Car Image"></td>
+                </tr>
+            </tbody>
         </table>
-        </div>
-        `
-    })
-    table.innerHTML = products.join('')
+    </div>
+        `;
+    });
+
+    table.innerHTML = products.join('');
 }
-joel()
-    // these are called nested functions
-    function idontknow() {
-        localStorage.setItem('dealership', JSON.stringify(dealership))
-        dealership = JSON.parse(localStorage.getItem('dealership'))
+
+// these are called nested functions
+function idontknow() {
+    localStorage.setItem('dealership', JSON.stringify(dealership));
+    dealership = JSON.parse(localStorage.getItem('dealership'));
+}
+
+function remove(position) {
+    dealership.splice(position, 1);
+    idontknow();
+    joel();
+}
+
+table.addEventListener('click', function (event) {
+    if (event.target.classList.contains('delete')) {
+        remove(event.target.value);
     }
-    function remove(index) {
-        
-        dealership.splice(index, 1)
-        idontknow()
-        joel()
+});
+
+let deletebutton = document.querySelector('.delete');
+
+table.style.color = 'white';
+
+// Event listener for my "Delete" button
+table.addEventListener('click', function (event) {
+    if (event.target.classList.contains('delete')) {
+        remove(event.target.value);
     }
-    
-    table.addEventListener('click', function (event) {
-        if (event.target.classList.contains('delete')) {
-            alert(event.target.value)
-            remove(event.target.value)
-            // alert(event.target.value)
-        }
-    })
-    
-    let deletebutton = document.querySelector('.delete')
-    
-table.style.color = 'white'
+});
+
+// Function to handle adding a new product
+function openModal(title, index) {
+    // Set modal title
+
+    // Update modal body content with product information
+    let modalBody = document.getElementById('modalBody');
+
+    if (title === 'Edit Product') {
+        let carToEdit = dealership[index];
+        modalBody.innerHTML = `
+            <p><b>Car Name:</b> <input type="text" id="newCarName" placeholder="Enter new car name" value="${carToEdit.carname}"></p>
+            <p><b>Model:</b> <input type="text" id="newModel" placeholder="Enter new model" value="${carToEdit.model}"></p>
+            <p><b>Year:</b> <input type="number" id="newYear" placeholder="Enter new year" value="${carToEdit.year}"></p>
+            <p><b>Price:</b> R <input type="number" id="newPrice" placeholder="Enter new price" value="${carToEdit.price}"></p>
+            <p><b>Image:</b> <input type="text" id="newUrl" placeholder="Enter new image URL" value="${carToEdit.url}"></p>
+        `;
+    }
+
+    // Show the modal
+    let infoModal = new bootstrap.Modal(document.getElementById('infoModal'));
+    infoModal.show();
+}
+
+// Function to handle saving changes
+function saveChanges() {
+    // Get values from modal input fields
+    let newCarName = document.getElementById('newCarName').value;
+    let newModel = document.getElementById('newModel').value;
+    let newYear = parseInt(document.getElementById('newYear').value);
+    let newPrice = parseInt(document.getElementById('newPrice').value);
+    let newUrl = document.getElementById('newUrl').value;
+
+    // Update the existing product
+    let carToEdit = dealership[editIndex];
+    carToEdit.carname = newCarName || carToEdit.carname;
+    carToEdit.model = newModel || carToEdit.model;
+    carToEdit.year = newYear || carToEdit.year;
+    carToEdit.price = newPrice || carToEdit.price;
+    carToEdit.url = newUrl || carToEdit.url;
+
+    // Save changes to local storage
+    idontknow();
+    joel();
+}
+
+// Variable to store the index of the car being edited
+let editIndex;
+
+// Event listener for "Edit" button
+table.addEventListener('click', function (event) {
+    if (event.target.classList.contains('edit')) {
+        editIndex = event.target.value;
+        openModal('Edit Product', editIndex);
+    }
+});
+
+// Event listener for "Save Changes" button in the modal
+document.getElementById('saveChangesBtn').addEventListener('click', saveChanges);
+
+// Event listener for opening the modal when "Add New Product" button is clicked
+document.getElementById('addNewButton').addEventListener('click', openAddModal);
+
+// Event listener for saving the new product when "Save New Product" button is clicked
+document.getElementById('saveNewProductBtn').addEventListener('click', function () {
+    saveNewProduct();
+    closeModal('addNewModal'); // Updated modal ID
+});
+
+// Function to open the add modal
+function openAddModal() {
+    document.querySelector('.modal-title').innerText = 'Add New Product';
+    document.getElementById('newCarName').value;
+    document.getElementById('newModel').value;
+    document.getElementById('newYear').value;
+    document.getElementById('newPrice').value;
+    document.getElementById('newUrl').value;
+    let addModal = new bootstrap.Modal(document.getElementById('addNewModal')); // Updated modal ID
+    addModal.show();
+}
+
+// Function to save the new product
+function saveNewProduct() {
+    let newCarName = document.getElementById('newCarName').value;
+    let newModel = document.getElementById('newModel').value;
+    let newYear = parseInt(document.getElementById('newYear').value);
+    let newPrice = parseInt(document.getElementById('newPrice').value);
+    let newUrl = document.getElementById('newUrl').value;
+
+    // Validate inputs and add new product
+    if (newCarName && newModel && !isNaN(newYear) && !isNaN(newPrice) && newUrl) {
+        let newCar = new Carconstructor(newCarName, newModel, newYear, newPrice, newUrl);
+        dealership.push(newCar);
+        idontknow();
+        joel();
+    } else {
+        alert('Please enter valid information for all fields.');
+    }
+}
+
+document.getElementById('closModal').addEventListener('click', closeModal);
+// Function to close the modal by ID
+function closeModal(modalId) {
+    let modal = new bootstrap.Modal(document.getElementById(modalId));
+    modal.hide();
+}
+
+// spinner function
+if (dealership.length === 0) {
+    table.innerHTML = `
+    <div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+    `;
+} else {
+    joel(dealership);
+}
--- Javascript/checkout.js
+++ Javascript/checkout.js
@@ -1,14 +1,35 @@
 let purchased = JSON.parse(localStorage.getItem('storeitems'))
 
-let table = document.querySelector('table')
-table.innerHTML= purchased.map((car,index)=>{
+let main = document.querySelector('main')
+main.innerHTML= purchased.map((car,index)=>{
     return `
-    <tr>
-    <td>${car.carname}</td>
-    <td>${car.model}</td>
-    <td>R${car.price}</td>
-    <td><img src='${car.url}' height="70px" width="80px"></img></td>
-    </tr>
+    <div class="table-responsive-lg">
+    <table class="table table-bordered table-hover">
+        <thead class="thead-dark">
+            <tr>
+                <th>Car Name</th>
+                <th>Model</th>
+                <th>Year</th>
+                <th>Price</th>
+                <th>Image</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>${car.carname}</td>
+                <td>${car.model}</td>
+                <td>${car.year}</td>
+                <td>R ${car.price}</td>
+                <td><img src="${car.url}" height="70px" width="80px" class="img-fluid" alt="Car Image"></td>
+                <td>
+                    <button class='btn btn-warning edit' value='${index}'>Edit</button>
+                    <button class='btn btn-danger delete' value='${index}'>Delete</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
     `
 }).join('')
 
